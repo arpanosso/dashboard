45de2d5d970f37f0f99f69d1b6c832b68ac6b4bb
Alan R Panosso <arpanosso@yahoo.com.br>
2021-06-07 21:26:34 -0300

    aula 04

--- pratica/15-shinydashboardMenu.R
+++ pratica/15-shinydashboardMenu.R
@@ -1,6 +1,9 @@
 library(shiny)
 library(shinydashboard)
+# https://fontawesome.com/
 
+
+##shiny::includeMarkdown("caminho do arquivo") # COPIA EM HTML DENTRO DO MARCKDONW
 
 ui <- dashboardPage(
   header=dashboardHeader(
@@ -8,8 +11,13 @@ ui <- dashboardPage(
   ),
   sidebar=dashboardSidebar(
     sidebarMenu(
-      menuItem("Infromações Gerais",tabName = "info"),
-      menuItem("Orçamentos",tabName = "orcamentos")
+      menuItem("Infromações Gerais",tabName = "info",icon = icon("twitter-square")),
+      menuItem("Orçamentos",tabName = "orcamentos",icon = icon("dollar-sign")),
+      menuItem("Receitas",
+               menuItem("visão 01",tabName = "visao_1"),
+               menuItem("visão 02",tabName = "visao_2",
+                        menuSubItem("Visao 02",tabName = "vis22"),
+                        menuSubItem("Visao 02",tabName = "vis22")))
     )
   ),
   body=dashboardBody(
@@ -27,6 +35,14 @@ ui <- dashboardPage(
       tabItem(
         tabName = "orcamentos",
         h1("Analisando os orçamentos")
+      ),
+      tabItem(
+        tabName = "visao_1",
+        h1("visão 01")
+      ),
+      tabItem(
+        tabName = "visao_2",
+        h1("visão 02")
       )
     )
   ),
